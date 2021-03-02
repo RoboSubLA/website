9c98bc7ca6c89da36f13efa3a2acddfd01b2985d
Ricardo Medina <rmedina0531@gmail.com>
2021-03-02 11:24:41 -0800

    initial commit. Working navbar

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+// import './App.scss'
+// import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap'
+import Navigation from './components/Navigation'
+
+
+function App() {
+  return(
+      <div className="App">
+        <Navigation/>
+     </div>
+  )
+}
+
+export default App;
