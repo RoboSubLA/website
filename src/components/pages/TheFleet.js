e9afe0369ce5e263ef36cebaac86c2552c11e908
gonzo-32 <gonzalez.heriberto97@gmail.com>
2021-03-24 19:07:38 -0700

    changes made

--- src/components/pages/TheFleet.js
+++ src/components/pages/TheFleet.js
@@ -11,15 +11,16 @@ const TheFleet = () => {
         <>
             <h1 style={{textAlign: 'center', fontFamily: 'Audiowide, sans-serif'}}>Meet the Fleet!!</h1>
 
+            {/* 2020-2021 */}
             <Jumbotron>
-                <h1>2019-2021</h1>
-                <h3>Rankings</h3>
-                <p>Overall: 11th</p>
-                <p>Video: 9th</p>
-                <p>Website: 13th</p>
-                <p>Technical-Design Report: 20th</p>
+                <h1>2020-2021 (Online-Competition)</h1>
+                <h3>Rankings:</h3>
+                <p>Overall: ~ </p>
+                <p>Video: ~ </p>
+                <p>Website: ~ </p>
+                <p>Technical-Design Report: ~</p>
             </Jumbotron>
-
+            
             <CardDeck>
                 <Card border="info" >
                     <Card.Img variant="top" src="https://www.calstatela.edu/sites/default/files/users/u227286/lanturn.png" />
@@ -51,13 +52,53 @@ const TheFleet = () => {
 
                 
             </CardDeck>
+
+            {/* 2019-2020 */}
+            <Jumbotron>
+                <h1>2019-2020 (Online-Competition)</h1>
+                <h3>Rankings:</h3>
+                <p>Overall: 11th</p>
+                <p>Video: 9th</p>
+                <p>Website: 13th</p>
+                <p>Technical-Design Report: 20th</p>
+            </Jumbotron>
+
+            <CardDeck>
+                <Card border="info" >
+                    <Card.Img variant="top" src="https://www.calstatela.edu/sites/default/files/users/u227286/lanturn.png" />
+                    <Card.Body>
+                        <Card.Title>Lanturn 2019-2021</Card.Title>
+                        <Card.Text>
+                            Complications Due to COVID-19...
+                        </Card.Text>
+                    </Card.Body>
+                    <Button variant="primary">Go somewhere</Button>
+                    <Card.Footer>
+                        <small className="text-muted">Last updated 3 mins ago</small>
+                    </Card.Footer>
+                </Card>
+
+                <Card border="info" >
+                    <Card.Img variant="top" src="https://www.calstatela.edu/sites/default/files/users/u227286/donphan.jpg" />
+                    <Card.Body>
+                        <Card.Title>Donphan 2019-2021</Card.Title>
+                        <Card.Text>
+                        Complications Due to COVID-19...
+                        </Card.Text>
+                    </Card.Body>
+                    <Button variant="primary">Go somewhere</Button>
+                    <Card.Footer>
+                        <small className="text-muted">Last updated 3 mins ago</small>
+                    </Card.Footer>
+                </Card>
+
+                
+            </CardDeck>
             
+            {/* 2018-2019 */}
             <Jumbotron>
                 <h1>2018-2019</h1>
-                <h3>Rankings</h3>
-                <p>Video</p>
-                <p>Website</p>
-                <p>Technical-Design Report</p>
+                <h3>Ranking: 31st</h3>
             </Jumbotron>
 
             <CardDeck>
