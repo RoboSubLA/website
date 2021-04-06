d1b49fb10198b78129aa47c742976f7751caf542
Ricardo Medina <rmedina0531@gmail.com>
2021-04-06 13:16:59 -0700

    added more Linux install resources

--- src/components/pages/Resources.js
+++ src/components/pages/Resources.js
@@ -1,95 +1,107 @@
-import React from 'react'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import Tab from 'react-bootstrap/Tab'
-import Nav from 'react-bootstrap/Nav'
-import './resources.css'
-import Footer from '../Footer'
-import Arduino from './resource-sections/Arduino'
-import ComputerVision from './resource-sections/ComputerVision'
-import ROS from './resource-sections/ROS'
+import React from "react";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Tab from "react-bootstrap/Tab";
+import Nav from "react-bootstrap/Nav";
+import "./resources.css";
+import Footer from "../Footer";
+
+// import resource sections
+import Arduino from "./resource-sections/Arduino";
+import ComputerVision from "./resource-sections/ComputerVision";
+import ROS from "./resource-sections/ROS";
+import Linux from "./resource-sections/Linux";
 
 const Resources = () => {
-    return (
-        <div>
-            {/* <h1 className='resource-title'>Resources</h1> */}
-            <Tab.Container id="left-tabs-example" defaultActiveKey="first">
-                <Row className='resource-row'>
-                    <Col className='resource-left-col' sm={3}>
-                        <Nav className='resource-item-container flex-column' variant="pills">
+  return (
+    <div>
+      {/* <h1 className='resource-title'>Resources</h1> */}
+      <Tab.Container id="left-tabs-example" defaultActiveKey="getstarted">
+        <Row className="resource-row">
+          <Col className="resource-left-col" sm={3}>
+            <Nav
+              className="resource-item-container flex-column"
+              variant="pills"
+            >
+              <Nav.Item className="resource-item">
+                <Nav.Link className="resource-tab" eventKey="getstarted">
+                  Get Started
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item className="resource-item">
+                <Nav.Link className="resource-tab" eventKey="arduino">
+                  Arduino
+                </Nav.Link>
+              </Nav.Item>
 
-                            <Nav.Item className='resource-item' >
-                                <Nav.Link  className='resource-tab' eventKey="arduino">Arduino</Nav.Link>
-                            </Nav.Item>
-                    
-                            <Nav.Item className='resource-item'>
-                                <Nav.Link eventKey="cv">Computer Vision</Nav.Link>
-                            </Nav.Item>
+              <Nav.Item className="resource-item">
+                <Nav.Link eventKey="cv">Computer Vision</Nav.Link>
+              </Nav.Item>
 
-                            <Nav.Item className='resource-item'>
-                                <Nav.Link eventKey="electrical">Electrical</Nav.Link>
-                            </Nav.Item>
+              <Nav.Item className="resource-item">
+                <Nav.Link eventKey="electrical">Electrical</Nav.Link>
+              </Nav.Item>
 
-                            <Nav.Item className='resource-item'>
-                                <Nav.Link eventKey="github">Git/Github</Nav.Link>
-                            </Nav.Item>
+              <Nav.Item className="resource-item">
+                <Nav.Link eventKey="github">Git/Github</Nav.Link>
+              </Nav.Item>
 
-                            <Nav.Item className='resource-item'>
-                                <Nav.Link eventKey="linux">Linux</Nav.Link>
-                            </Nav.Item>
+              <Nav.Item className="resource-item">
+                <Nav.Link eventKey="linux">Linux</Nav.Link>
+              </Nav.Item>
 
-                            <Nav.Item className='resource-item'>
-                                <Nav.Link eventKey="mechanical">Mechanical</Nav.Link>
-                            </Nav.Item>
+              <Nav.Item className="resource-item">
+                <Nav.Link eventKey="mechanical">Mechanical</Nav.Link>
+              </Nav.Item>
 
-                            <Nav.Item className='resource-item'> 
-                                <Nav.Link eventKey="ros">Ros</Nav.Link>
-                            </Nav.Item>
+              <Nav.Item className="resource-item">
+                <Nav.Link eventKey="ros">Ros</Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Col>
 
-                        </Nav>
-                    </Col>
+          <Col sm={9} className="resource-container">
+            <Tab.Content>
+              <Tab.Pane eventKey="arduino">
+                <h1 className="resource-text-title">Arduino</h1>
+                <Arduino />
+              </Tab.Pane>
 
-                    <Col sm={9} className='resource-container'>
-                        <Tab.Content>
-                            
-                            <Tab.Pane eventKey="arduino">
-                                <h1 className='resource-text-title'>Arduino</h1>
-                                <Arduino />
-                            </Tab.Pane>
+              <Tab.Pane eventKey="cv">
+                <h1 className="resource-text-title">Computer Vision</h1>
+                <ComputerVision />
+              </Tab.Pane>
 
-                            <Tab.Pane eventKey="cv">
-                                <h1 className='resource-text-title'>Computer Vision</h1>
-                                <ComputerVision/>
-                            </Tab.Pane>
-                            
-                            <Tab.Pane eventKey="electrical">
-                                <h2 className='resource-text-title'>Even More Stuff</h2>
-                            </Tab.Pane>
-                           
-                            <Tab.Pane eventKey="github">
-                            <h2 className='resource-text-title'>Less Stuff</h2>
-                            </Tab.Pane>
-                           
-                            <Tab.Pane eventKey="linux">
-                            <h2 className='resource-text-title'>Less Stuff</h2>
-                            </Tab.Pane>
-                            
-                            <Tab.Pane eventKey="mechanical">
-                                <h2 className='resource-text-title'>Less Stuff</h2>
-                            </Tab.Pane>
-                           
-                            <Tab.Pane eventKey="ros">
-                                <h2 className='resource-text-title'>Robotic Operating System</h2>
-                                <ROS/>
-                            </Tab.Pane>
+              <Tab.Pane eventKey="electrical">
+                <h2 className="resource-text-title">Even More Stuff</h2>
+              </Tab.Pane>
 
-                        </Tab.Content>
-                    </Col>
-                </Row>
-            </Tab.Container>
-            <Footer/>
-        </div>
-    )
-}
+              <Tab.Pane eventKey="github">
+                <h2 className="resource-text-title">Less Stuff</h2>
+              </Tab.Pane>
 
-export default Resources
+              <Tab.Pane eventKey="linux">
+                <h2 className="resource-text-title">Linux</h2>
+                <Linux />
+              </Tab.Pane>
+
+              <Tab.Pane eventKey="mechanical">
+                <h2 className="resource-text-title">Less Stuff</h2>
+              </Tab.Pane>
+
+              <Tab.Pane eventKey="ros">
+                <h2 className="resource-text-title">
+                  Robotic Operating System
+                </h2>
+                <ROS />
+              </Tab.Pane>
+            </Tab.Content>
+          </Col>
+        </Row>
+      </Tab.Container>
+      <Footer />
+    </div>
+  );
+};
+
+export default Resources;
---
+++ src/components/pages/resource-sections/Linux.js
@@ -0,0 +1,97 @@
+import React from "react";
+
+const Linux = () => {
+  return (
+    <>
+      <h1>Installing Linux </h1>
+      <p>When it comes to installing ross there are 3 main options</p>
+
+      <li>Virtualization</li>
+      <li>Native Install on External Hard Drive</li>
+      <li>Hard Drive Partitioning</li>
+      <br></br>
+
+      <h2>Virtualization</h2>
+      <p>
+        Virtualization involves installing an emulator to run an instance of the
+        Linux os inside a window on a host computer. The main benifit of this
+        type of install is the ability to add and remove various versions of
+        OS's without the need of making any major change to your hard drive. It
+        is the easiest to implement. The downsides include needing at least 8gb
+        of ram for the set up we are running, as well as needing different
+        install software depending on your native OS. In addition the computer
+        vision software requirements are different, and running those algorithms
+        might produce unexpected results, or refuse to run altogether. Still a
+        great choice if Linus is new to you and want to try it out risk free.
+      </p>
+
+      <a href="https://www.makeuseof.com/tag/install-linux-windows-vmware-virtual-machine/">
+        Install Linux on windows
+      </a>
+      <br></br>
+      <a href="https://www.imore.com/how-use-linux-your-mac-using-virtual-machine">
+        Install Linux on MAC
+      </a>
+      <br></br>
+
+      <h2>External Hard Drive Install</h2>
+      <p>
+        Installing on an external hard drive is the easiest way to have your
+        host computer nativily boot linux. The only required material is an
+        additional hard drive (internal or external) and a bootable USB
+      </p>
+      <h4>Create a Bootable USB Windows</h4>
+      <p className="iframe-align">
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/xnisuFk-cDg"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+      </p>
+      <h4>Create a Bootable USB MAC</h4>
+      <p className="iframe-align">
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/Yf3VChom8qo"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+      </p>
+      <br></br>
+
+      <a href="https://ubuntu.com/tutorials/install-ubuntu-desktop#1-overview">
+        Install Linux using bootable USB
+      </a>
+      <p>
+        Make sure when selecting the hard drive to install Linux it is the
+        external drive and not your host OS.
+      </p>
+      <h2>Hard Drive Partitioning</h2>
+      <p>
+        This method involves partitioning your hard drive on the host computer
+        to allow seperate OS's to be installed on the same drive. It has the
+        least computer requirements, as Linux is installed on the hard drive and
+        uses all host resources. Partitioning is potentially risky as there is a
+        possibility of deleting your operating system but as long as you are
+        paying attention and following steps there should be no problems.
+      </p>
+      <a href="https://itsfoss.com/install-ubuntu-1404-dual-boot-mode-windows-8-81-uefi/">
+        Partition Install on Windows
+      </a>
+      <br></br>
+      <a href="https://www.makeuseof.com/tag/install-linux-macbook-pro/">
+        Partition Install on Mac
+      </a>
+      <br></br>
+    </>
+  );
+};
+
+export default Linux;
