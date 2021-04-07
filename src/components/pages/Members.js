8b72a30406e69c34c74397e04b78bbfcfdacae0f
Ricardo Medina <rmedina0531@gmail.com>
2021-04-07 13:11:37 -0700

    changed members json and component to reflect yearly changes

--- src/components/pages/Members.js
+++ src/components/pages/Members.js
@@ -1,105 +1,166 @@
-import React from 'react'
-import MemberSection from '../MemberSection'
-import Card from 'react-bootstrap/Card'
-import CardDeck from 'react-bootstrap/CardDeck'
-import Button from 'react-bootstrap/Button'
-import Footer from '../Footer'
-import '../pages/members.css'
+import React from "react";
+import MemberSection from "../MemberSection";
+import Card from "react-bootstrap/Card";
+import CardDeck from "react-bootstrap/CardDeck";
+import Button from "react-bootstrap/Button";
+import Footer from "../Footer";
+import "../pages/members.css";
 // import '../../App.css'
 
 const Members = () => {
-    let json = require('../../data/members.json')
-    return( 
-        <>  
-            
+  // add members for the year to the JSON under the members directory under a new year
+  let obj = require("../../data/members.json");
+  let json = obj[Object.keys(obj)[0]];
+  return (
+    <>
+      <div className="members-container">
+        <h1
+          className="officer-tag"
+          style={{ textAlign: "center", fontFamily: "Audiowide, sans-serif" }}
+        >
+          MEET THE OFFICERS
+        </h1>
 
-            <div className='members-container'>
-               
-                <h1 className='officer-tag' style={{textAlign: 'center', fontFamily: 'Audiowide, sans-serif'}}>MEET THE OFFICERS</h1>
+        <CardDeck className="card-deck-members">
+          <Card className="card-members" text="light" bg="dark">
+            <Card.Img
+              className="card-member-img"
+              variant="top"
+              src={json.officers[0].img}
+            />
+            <Card.Body className="card-member-body">
+              <Card.Title className="card-member-name">
+                {json.officers[0].name}
+              </Card.Title>
+              <Card.Title className="card-member-title">
+                {json.officers[0].title}
+              </Card.Title>
+              {/* <Card.Text>{json.officers[0].text}</Card.Text> */}
+              <Button
+                className="card-member-button-officers"
+                variant="outline-light"
+                href={json.officers[0].link}
+              >
+                More Info
+              </Button>
+            </Card.Body>
+          </Card>
 
-                <CardDeck className='card-deck-members'>
+          <Card className="card-members" text="light" bg="dark">
+            <Card.Img
+              className="card-member-img"
+              variant="top"
+              src={json.officers[1].img}
+            />
+            <Card.Body className="card-member-body">
+              <Card.Title className="card-member-name">
+                {json.officers[1].name}
+              </Card.Title>
+              <Card.Title className="card-member-title">
+                {json.officers[1].title}
+              </Card.Title>
+              {/* <Card.Text>{json.officers[1].text}</Card.Text> */}
+              <Button
+                className="card-member-button-officers"
+                variant="outline-light"
+                href={json.officers[1].link}
+              >
+                More Info
+              </Button>
+            </Card.Body>
+          </Card>
+        </CardDeck>
 
-                    <Card className='card-members' text='light' bg='dark'>
-                        <Card.Img className='card-member-img' variant="top" src={json.officers[0].img} />
-                        <Card.Body className='card-member-body'>
-                            <Card.Title className='card-member-name'>{json.officers[0].name}</Card.Title>
-                            <Card.Title className='card-member-title'>{json.officers[0].title}</Card.Title>
-                            {/* <Card.Text>{json.officers[0].text}</Card.Text> */}
-                            <Button className='card-member-button-officers' variant="outline-light" href={json.officers[0].link}>More Info</Button>
-                        
-                        </Card.Body>
-                    
-                    </Card>
+        <CardDeck className="card-deck-members">
+          <Card className="card-members" text="light" bg="dark">
+            <Card.Img
+              className="card-member-img"
+              variant="top"
+              src={json.officers[4].img}
+            />
+            <Card.Body className="card-member-body">
+              <Card.Title className="card-member-name">
+                {json.officers[4].name}
+              </Card.Title>
+              <Card.Title className="card-member-title">
+                {json.officers[4].title}
+              </Card.Title>
 
-                    <Card className='card-members' text='light' bg='dark'>
-                        <Card.Img className='card-member-img' variant="top" src={json.officers[1].img} />
-                        <Card.Body className='card-member-body'>
-                            <Card.Title className='card-member-name'>{json.officers[1].name}</Card.Title>
-                            <Card.Title className='card-member-title'>{json.officers[1].title}</Card.Title>
-                            {/* <Card.Text>{json.officers[1].text}</Card.Text> */}
-                            <Button className='card-member-button-officers' variant="outline-light" href={json.officers[1].link}>More Info</Button>
-                        </Card.Body>
-                        
-                        
-                    </Card>
+              {/* <Card.Text>{json.officers[4].text}</Card.Text> */}
+              <Button
+                className="card-member-button-officers"
+                variant="outline-light"
+                href={json.officers[4].link}
+              >
+                More Info
+              </Button>
+            </Card.Body>
+          </Card>
 
-                </CardDeck>
+          <Card className="card-members" text="light" bg="dark">
+            <Card.Img
+              className="card-member-img"
+              variant="top"
+              src={json.officers[2].img}
+            />
+            <Card.Body className="card-member-body">
+              <Card.Title className="card-member-name">
+                {json.officers[2].name}
+              </Card.Title>
+              <Card.Title className="card-member-title">
+                {json.officers[2].title}
+              </Card.Title>
 
-                <CardDeck className='card-deck-members'>
-            
+              {/* <Card.Text>{json.officers[2].text}</Card.Text> */}
 
-                    <Card className='card-members' text='light' bg='dark' >
-                        <Card.Img className='card-member-img' variant="top" src={json.officers[4].img} />
-                        <Card.Body className='card-member-body'>
-                            <Card.Title className='card-member-name'>{json.officers[4].name}</Card.Title>
-                            <Card.Title className='card-member-title'>{json.officers[4].title}</Card.Title>
-                            
-                            {/* <Card.Text>{json.officers[4].text}</Card.Text> */}
-                            <Button className='card-member-button-officers' variant="outline-light" href={json.officers[4].link}>More Info</Button>
-                        </Card.Body>
-                        
-                        
-                    </Card>
+              <Button
+                className="card-member-button-officers"
+                variant="outline-light"
+                href={json.officers[2].link}
+              >
+                More Info
+              </Button>
+            </Card.Body>
+          </Card>
 
-                    <Card className='card-members' text='light' bg='dark' >
-                        <Card.Img className='card-member-img' variant="top" src={json.officers[2].img} />
-                        <Card.Body  className='card-member-body'>
-                            <Card.Title className='card-member-name'>{json.officers[2].name}</Card.Title>
-                            <Card.Title className='card-member-title'>{json.officers[2].title}</Card.Title>
-                            
-                            {/* <Card.Text>{json.officers[2].text}</Card.Text> */}
+          <Card className="card-members" text="light" bg="dark">
+            <Card.Img
+              className="card-member-img"
+              variant="top"
+              src={json.officers[3].img}
+            />
+            <Card.Body className="card-member-body">
+              <Card.Title className="card-member-name">
+                {json.officers[3].name}
+              </Card.Title>
+              <Card.Title className="card-member-title">
+                {json.officers[3].title}
+              </Card.Title>
+              {/* <Card.Text>{json.officers[3].text}</Card.Text> */}
 
-                            <Button className='card-member-button-officers' variant="outline-light" href={json.officers[2].link}>More Info</Button>
-                        </Card.Body>
-                        
-                        
-                    </Card>
+              <Button
+                className="card-member-button-officers"
+                variant="outline-light"
+                href={json.officers[3].link}
+              >
+                More Info
+              </Button>
+            </Card.Body>
+          </Card>
+        </CardDeck>
 
-                    <Card className='card-members' text='light' bg='dark' >
-                        <Card.Img className='card-member-img' variant="top" src={json.officers[3].img} />
-                        <Card.Body  className='card-member-body'>
-                            <Card.Title className='card-member-name'>{json.officers[3].name}</Card.Title>
-                            <Card.Title className='card-member-title'>{json.officers[3].title}</Card.Title>
-                            {/* <Card.Text>{json.officers[3].text}</Card.Text> */}
+        <h1
+          className="officer-tag"
+          style={{ textAlign: "center", fontFamily: "Audiowide, sans-serif" }}
+        >
+          MEET THE MEMBERS
+        </h1>
 
-                        <Button className='card-member-button-officers' variant="outline-light" href={json.officers[3].link}>More Info</Button>
-                        
-                        </Card.Body>
-                        
-                    </Card>
+        <MemberSection list={json.members} />
+      </div>
+      <Footer />
+    </>
+  );
+};
 
-                </CardDeck>
-
-
-
-                <h1 className='officer-tag' style={{textAlign: 'center', fontFamily: 'Audiowide, sans-serif'}}>MEET THE MEMBERS</h1>
-
-                <MemberSection list={json.members}/>
-                
-            </div>   
-            <Footer/> 
-        </>  
-    )
-}
-
-export default Members
+export default Members;
