ba5ad0120e3d94960ff3742b0ade62323d657e72
Ricardo Medina <rmedina0531@gmail.com>
2021-04-08 11:55:46 -0700

    youtube button functionality added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,45 +1,43 @@
-import React from 'react';
-import './Footer.css';
-import { Button } from './Button';
-import { Link } from 'react-router-dom';
+import React from "react";
+import "./Footer.css";
+import { Button } from "./Button";
+import { Link } from "react-router-dom";
 
 function Footer() {
   return (
-    <div className='footer-container'>
-      <section class='social-media'>
-        <div class='social-media-wrap'>
-          <div class='footer-logo'>
-            <Link to='/' className='social-logo'>
+    <div className="footer-container">
+      <section class="social-media">
+        <div class="social-media-wrap">
+          <div class="footer-logo">
+            <Link to="/" className="social-logo">
               AUV
-              <i class='fas fa-water' />
+              <i class="fas fa-water" />
             </Link>
           </div>
-          <small class='website-rights'>Robosub © 2020</small>
-          <div class='social-icons'>
-            
+          <small class="website-rights">Robosub © 2020</small>
+          <div class="social-icons">
             <Link
-              class='social-icon-link instagram'
-              to='/'
-              target='_blank'
-              aria-label='Instagram'
+              class="social-icon-link instagram"
+              to="/"
+              target="_blank"
+              aria-label="Instagram"
             >
-              <i class='fab fa-instagram' />
+              <i class="fab fa-instagram" />
             </Link>
-            <Link
-              class='social-icon-link youtube'
-              to='/'
-              target='_blank'
-              aria-label='Youtube'
+            <a
+              className="social-icon-link youtube"
+              href="https://www.youtube.com/channel/UC4nhzXkauvCcCjmJ-_1YL3Q"
+              aria-label="Youtube"
             >
-              <i class='fab fa-youtube' />
-            </Link>
+              <i class="fab fa-youtube" />
+            </a>
             <Link
-              class='social-icon-link twitter'
-              to='/'
-              target='_blank'
-              aria-label='Twitter'
+              class="social-icon-link twitter"
+              to="/"
+              target="_blank"
+              aria-label="Twitter"
             >
-              <i class='fab fa-twitter' />
+              <i class="fab fa-twitter" />
             </Link>
           </div>
         </div>
