b54c64d47ffc7de77d6d2fa0c4c865b1b5170e54
gonzo-32 <gonzalez.heriberto97@gmail.com>
2021-04-07 21:18:39 -0700

    more progress on new members page

--- src/components/MemberSection.js
+++ src/components/MemberSection.js
@@ -1,47 +1,36 @@
 import React from 'react'
 import Figure from 'react-bootstrap/Figure'
 import Button from 'react-bootstrap/Button'
-import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 
 import './pages/members.css'
 
 const Member = (props) => {
     return (
-        // <Card className='card-members' border="info" >
-        //     <Card.Img className='card-member-img' variant="top" src={props.json.img} />
-        //     <Card.Body className='card-member-body'>
-        //         {/* <Card.Title>{props.json.position}</Card.Title> */}
-        //         <Card.Text className='card-member-name'>{props.json.name}</Card.Text>
-        //         {/* <Card.Text>{props.json.text}</Card.Text> */}
-        //         <Button className='card-member-button' variant="outline-dark" href={props.json.link}>More Info</Button>
-        //     </Card.Body>
-            
-            
-        // </Card>
-        <Figure>
-            <Figure.Image
-                width={171}
-                height={180}
-                alt="171x180"
-                src={props.json.img}
-            />
-            <Figure.Caption>
-                    {props.json.name}
-            </Figure.Caption>
-        </Figure>
+        <Col sm>
+           <Figure className='members-container'>
+                <Figure.Image
+                    className='members-img'
+                    width={171}
+                    height={180}
+                    alt="171x180"
+                    src={props.json.img}
+                />
+                <Figure.Caption>
+                        {props.json.name}
+                </Figure.Caption>
+            </Figure> 
+        </Col>
+        
     )
 }
 
 const MemberSection = (props) => {
     return (
         <>
-        <Col sm>
             {props.list.map((item) => {
                 return (<Member json={item} key={item.name}/>)
             })}
-        </Col>
-
         </>
         
     )
--- src/components/pages/Members.js
+++ src/components/pages/Members.js
@@ -4,7 +4,7 @@ import Figure from 'react-bootstrap/Figure'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
-
+import MemberSection from '../MemberSection'
 
 const Members = () => {
     let json = require('../../data/members.json')
@@ -13,8 +13,9 @@ const Members = () => {
             <h1>Officers</h1>
             <Row>
                 <Col sm>
-                    <Figure>
+                      <Figure className='members-container'>
                     <Figure.Image
+                        className='members-img'
                         width={171}
                         height={180}
                         alt="171x180"
@@ -23,12 +24,17 @@ const Members = () => {
                         <Figure.Caption>
                             {json.officers[0].name}
                         </Figure.Caption>
-                    </Figure>
+                        <Figure.Caption>
+                            {json.officers[0].title}
+                        </Figure.Caption>
+
+                    </Figure>  
                 </Col>
 
                 <Col sm>
-                    <Figure>
+                    <Figure className='members-container'>
                     <Figure.Image
+                        className='members-img'
                         width={171}
                         height={180}
                         alt="171x180"
@@ -37,12 +43,16 @@ const Members = () => {
                         <Figure.Caption >
                             {json.officers[1].name}
                         </Figure.Caption>
+                        <Figure.Caption>
+                            {json.officers[1].title}
+                        </Figure.Caption>
                     </Figure>
                 </Col>
 
                 <Col sm>
-                    <Figure>
+                    <Figure className='members-container'>
                     <Figure.Image
+                        className='members-img'
                         width={171}
                         height={180}
                         alt="171x180"
@@ -51,12 +61,16 @@ const Members = () => {
                         <Figure.Caption >
                             {json.officers[2].name} 
                         </Figure.Caption>
+                        <Figure.Caption>
+                            {json.officers[2].title}
+                        </Figure.Caption>
                     </Figure>
                 </Col>
 
                 <Col sm>
-                    <Figure>
+                    <Figure className='members-container'>
                     <Figure.Image
+                        className='members-img'
                         width={171}
                         height={180}
                         alt="171x180"
@@ -65,12 +79,16 @@ const Members = () => {
                         <Figure.Caption >
                             {json.officers[3].name}
                         </Figure.Caption>
+                        <Figure.Caption>
+                            {json.officers[3].title}
+                        </Figure.Caption>
                     </Figure>
                 </Col>
 
                 <Col sm>
-                    <Figure>
+                    <Figure className='members-container'>
                     <Figure.Image
+                        className='members-img'
                         width={171}
                         height={180}
                         alt="171x180"
@@ -79,13 +97,83 @@ const Members = () => {
                         <Figure.Caption>
                             {json.officers[4].name}
                         </Figure.Caption>
+                        <Figure.Caption>
+                            {json.officers[4].title}
+                        </Figure.Caption>
                     </Figure>
                 </Col>
                 
             </Row>
-            <Row>
-                
+
+            <h1>Club Members</h1>
+            <Row>          
+            <MemberSection list={json.members}/>
             </Row>
+
+            <h1>Senior Design Team</h1>
+            <Row>          
+            <MemberSection list={json.seniordesign}/>
+            </Row>
+
+            <h1>Advisors</h1>
+            <Row>          
+                <Col sm>
+                    <Figure className='members-container'>
+                    <Figure.Image
+                        className='members-img'
+                        width={171}
+                        height={180}
+                        alt="171x180"
+                        src={json.advisors[0].img}
+                    />
+                        <Figure.Caption >
+                            {json.advisors[0].name}
+                        </Figure.Caption>
+                        <Figure.Caption>
+                            {json.advisors[0].title}
+                        </Figure.Caption>
+                    </Figure>
+                </Col>
+
+                <Col sm>
+                    <Figure className='members-container'>
+                    <Figure.Image
+                        className='members-img'
+                        width={171}
+                        height={180}
+                        alt="171x180"
+                        src={json.advisors[1].img}
+                    />
+                        <Figure.Caption>
+                            {json.advisors[1].name}
+                        </Figure.Caption>
+                        <Figure.Caption>
+                            {json.advisors[1].title}
+                        </Figure.Caption>
+                    </Figure>
+                </Col>
+
+                <Col sm>
+                    <Figure className='members-container'>
+                    <Figure.Image
+                        className='members-img'
+                        width={171}
+                        height={180}
+                        alt="171x180"
+                        src={json.advisors[2].img}
+                    />
+                        <Figure.Caption>
+                            {json.advisors[2].name}
+                        </Figure.Caption>
+                        <Figure.Caption>
+                            {json.advisors[2].title}
+                        </Figure.Caption>
+                    </Figure>
+                </Col>
+            </Row>
+
+
+
         </Container>
     )
 }
