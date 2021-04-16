5c19306564842241958567fd7e88b84d177eb59b
gonzo-32 <gonzalez.heriberto97@gmail.com>
2021-04-15 18:09:03 -0700

    lanturns me page almost done

--- src/pages/subpages/Lanturn.js
+++ src/pages/subpages/Lanturn.js
@@ -1,7 +1,8 @@
 import React from "react";
 import {Tabs, Tab, Jumbotron, Image, Carousel, Row, Figure, Col, Container }from "react-bootstrap";
-
+import Typical from "react-typical";
 import "./sub.css";
+
 const Member = (props) => {
   return (
     <Col className="figure-container" sm>
@@ -56,7 +57,7 @@ const Lanturn = () => {
         <Tabs defaultActiveKey="team" className='sub-tabs' id="uncontrolled-tab-example">
         
         <Tab eventKey="team" title="Team">
-          <Jumbotron>
+          <Jumbotron className='sub-team-jumbo'>
             <Container>
               <h1>The Team</h1>
               <Row className="members-row">
@@ -71,63 +72,83 @@ const Lanturn = () => {
         </Tab>
 
         <Tab eventKey="ME" title="Mechanical">
-          <Jumbotron>
-            <Container>
+          <Jumbotron className='sub-jumbo'>
+            {/* 2020-2021 */}
+            <div className='sub-container'>
+              <p className='sub-year'>
+                Year: {" "}
+                <Typical
+                  className='sub-year-animated'
+                  steps={["2020-2021", 5000, "", 2000]}
+                  loop={Infinity}
+                  wrapper="b"
+                />
+              </p>
 
-              <h1>2020-2021</h1>
               <Row>
+                {/* Frame and Haul */}
                 <Col>
-                  {/* 2019-2020 */}
-                  <h1>Frame & Haul</h1>
-                  <Row> 
-                    <Image src='./images/subs/lanturn/lanturn2.png ' width='400px' height='400px' />
+                  <h1 className='sub-h1'>FRAME & HAUL</h1>
+                  <Row className='sub-row-imgs'> 
+                    <Image  src='./images/subs/lanturn/lanturntopview.png ' width='400px' height='400px' />
                     <Image src='./images/subs/lanturn/lanturnframe.png ' width='400px' height='400px' />
                   </Row> 
-                  <p>The mechanical design for CSULA’s Lanturn submarine was focused on ease of manufacture and maintenance. Key design features are a box-shaped hull with a removable electronics shelving unit and a frame with slotted mounting points for modular mounting of subassemblies. Lanturn’s hull primary structure is formed by TIG welded sheets of AL 6061-T6. The lid is made of transparent acrylic, and seals to the hull primary structure through a peripheral nitrile gasket compressed by latches. Lanturn is outfitted with forward and downward Lanturn framefacing cameras and features a rear connector plate populated with a standardized set of Seacon connectors. The electronics shelving unit is made of acrylic sheets laser cut to shape/size with equally spaced mounting holes that span each sheet. An internal connector plate and connectorized electronics allow the shelving unit to be unplugged and removed from the hull. 90 degree snapping latches allow the shelves to be laid flat outside of the sub for easy maintenance and troubleshooting.</p>
+                  <p className='sub-p'>Indescribable oppression, which seemed to generate in some unfamiliar part of her consciousness, filled her whole being with a vague anguish. It was like a shadow, like a mist passing across her soul's summer day. It was strange and unfamiliar; it was a mood. She did not sit there inwardly upbraiding her husband, lamenting at Fate, which had directed her footsteps to the path which they had taken. She was just having a good cry all to herself. The mosquitoes made merry over her, biting her firm, round arms and nipping at her bare insteps.</p>
                 </Col>
               </Row>
 
               <Row>
+                {/* Actuated Systems */}
                 <Col>
-                 
-                  <h1>Actuated Systems</h1>
-                  <Row> 
-                    <Image  src='./images/subs/lanturn/lanturntopview.png ' width='400px' height='400px' />
-                    <Image src='./images/subs/lanturn/lanturntrans.png' width='400px' height='400px' />
+                  <h1 className='sub-h1'>ACTUATED SYSTEMS</h1>
+                  <Row className='sub-row-imgs'> 
+                    <Image src='./images/subs/lanturn/2019lanturntorpedo.png' width='300px' height='300px'/>
+                    <Image src='./images/subs/lanturn/2019lanturnclaw.png' width='300px' height='300px'/>
+                    <Image src='./images/subs/lanturn/2019lanturndropper.png' width='300px' height='300px'/>
                   </Row> 
-                  <p>The mechanical design for CSULA’s Lanturn submarine was focused on ease of manufacture and maintenance. Key design features are a box-shaped hull with a removable electronics shelving unit and a frame with slotted mounting points for modular mounting of subassemblies. Lanturn’s hull primary structure is formed by TIG welded sheets of AL 6061-T6. The lid is made of transparent acrylic, and seals to the hull primary structure through a peripheral nitrile gasket compressed by latches. Lanturn is outfitted with forward and downward Lanturn framefacing cameras and features a rear connector plate populated with a standardized set of Seacon connectors. The electronics shelving unit is made of acrylic sheets laser cut to shape/size with equally spaced mounting holes that span each sheet. An internal connector plate and connectorized electronics allow the shelving unit to be unplugged and removed from the hull. 90 degree snapping latches allow the shelves to be laid flat outside of the sub for easy maintenance and troubleshooting.</p>
+                  <p className='sub-p'>Indescribable oppression, which seemed to generate in some unfamiliar part of her consciousness, filled her whole being with a vague anguish. It was like a shadow, like a mist passing across her soul's summer day. It was strange and unfamiliar; it was a mood. She did not sit there inwardly upbraiding her husband, lamenting at Fate, which had directed her footsteps to the path which they had taken. She was just having a good cry all to herself. The mosquitoes made merry over her, biting her firm, round arms and nipping at her bare insteps.</p>
                 </Col>
               </Row>
+            </div>
 
-            </Container>
+            {/* 2019-2020 */}
+            <div className='sub-container' >
+              <p className='sub-year'>
+                Year: {" "}
+                <Typical
+                  className='sub-year-animated'
+                  steps={["2019-2020", 5000, "", 2000]}
+                  loop={Infinity}
+                  wrapper="b"
+                  
+                />
+              </p>
 
-
-            <Container>
-              <h1>2019-2020</h1>
               <Row>
+                {/* Frame and Haul */}
                 <Col>
-                  {/* 2019-2020 */}
-                  <h1>Frame & Haul</h1>
-                  <Row> 
+                  <h1 className='sub-h1'>FRAME & HAUL</h1>
+                  <Row className='sub-row-imgs'> 
                     <Image src='./images/subs/lanturn/2019lanturninsidehaul.png' width='400px' height='400px' />
                     <Image src='./images/subs/lanturn/2019lanturnframe.jpg' width='400px' height='400px' />
                   </Row> 
-                  <p>The mechanical design for CSULA’s Lanturn submarine was focused on ease of manufacture and maintenance. Key design features are a box-shaped hull with a removable electronics shelving unit and a frame with slotted mounting points for modular mounting of subassemblies. Lanturn’s hull primary structure is formed by TIG welded sheets of AL 6061-T6. The lid is made of transparent acrylic, and seals to the hull primary structure through a peripheral nitrile gasket compressed by latches. Lanturn is outfitted with forward and downward Lanturn framefacing cameras and features a rear connector plate populated with a standardized set of Seacon connectors. The electronics shelving unit is made of acrylic sheets laser cut to shape/size with equally spaced mounting holes that span each sheet. An internal connector plate and connectorized electronics allow the shelving unit to be unplugged and removed from the hull. 90 degree snapping latches allow the shelves to be laid flat outside of the sub for easy maintenance and troubleshooting.</p>
+                  <p className='sub-p'>The mechanical design for CSULA’s Lanturn submarine was focused on ease of manufacture and maintenance. Key design features are a box-shaped hull with a removable electronics shelving unit and a frame with slotted mounting points for modular mounting of subassemblies. Lanturn’s hull primary structure is formed by TIG welded sheets of AL 6061-T6. The lid is made of transparent acrylic, and seals to the hull primary structure through a peripheral nitrile gasket compressed by latches. Lanturn is outfitted with forward and downward Lanturn framefacing cameras and features a rear connector plate populated with a standardized set of Seacon connectors. The electronics shelving unit is made of acrylic sheets laser cut to shape/size with equally spaced mounting holes that span each sheet. An internal connector plate and connectorized electronics allow the shelving unit to be unplugged and removed from the hull. 90 degree snapping latches allow the shelves to be laid flat outside of the sub for easy maintenance and troubleshooting.</p>
                 </Col>
               </Row>
-                {/* 2019-2020 */}
-                <h1>Actuated Systems</h1>
-                <Row>
+                
+              <Row>
+                  {/* Actuated Systems */}
                   <Col>
-                    <Row> 
+                    <h1 className='sub-h1'>ACTUATED SYSTEMS</h1>
+                    <Row className='sub-row-imgs'> 
                       <Image src='./images/subs/lanturn/2019lanturntorpedo.png' width='300px' height='300px'/>
                       <Image src='./images/subs/lanturn/2019lanturnclaw.png' width='300px' height='300px'/>
                       <Image src='./images/subs/lanturn/2019lanturndropper.png' width='300px' height='300px'/>
                     </Row> 
-                    <p>One design criteria requires that components such as sensors and thrusters be easily added and removed in few steps to minimize time spent on maintenance, in return allocating more time to underwater testing. This requirement was met by creating a modular frame that acts as a hub for mounting components, as shown in the frame image. The design uses a combination of 6061-t6 aluminum t-slot extrusions and flat bars Clawto avoid complex machining, which contributes to ease of scaling if more mounting surfaces are needed. Components are slid into the t-slot fixture brackets then held in place by tightening one or more set screws.In addition to the frame and hull, lanturn features a claw, pneumatic torpedo system, and a DC motor controlled dropper. The claw uses a servo motor to rotate the gears, which are used to open and close the grippers. The grippers hold the objects in place after picking them up. The torpedo system uses a CO2 bucket changer for easy replacement of CO2 cartridges, an ASA regulator Dropper​to decrease the CO2 pressure to the desired value, a solenoid valve to release the CO2 to launch the torpedo, and the 3D printed torpedoes themselves. The dropper uses a DC motor to rotate the platform 90 degrees at a time to release 3 markers one at a time. The lid is used to replace the markers after each run. </p>
+                    <p className='sub-p'>One design criteria requires that components such as sensors and thrusters be easily added and removed in few steps to minimize time spent on maintenance, in return allocating more time to underwater testing. This requirement was met by creating a modular frame that acts as a hub for mounting components, as shown in the frame image. The design uses a combination of 6061-t6 aluminum t-slot extrusions and flat bars Clawto avoid complex machining, which contributes to ease of scaling if more mounting surfaces are needed. Components are slid into the t-slot fixture brackets then held in place by tightening one or more set screws.In addition to the frame and hull, lanturn features a claw, pneumatic torpedo system, and a DC motor controlled dropper. The claw uses a servo motor to rotate the gears, which are used to open and close the grippers. The grippers hold the objects in place after picking them up. The torpedo system uses a CO2 bucket changer for easy replacement of CO2 cartridges, an ASA regulator Dropper​to decrease the CO2 pressure to the desired value, a solenoid valve to release the CO2 to launch the torpedo, and the 3D printed torpedoes themselves. The dropper uses a DC motor to rotate the platform 90 degrees at a time to release 3 markers one at a time. The lid is used to replace the markers after each run. </p>
                   </Col>               
                 </Row>
-            </Container>
+            </div>
           </Jumbotron>
         </Tab>
 
