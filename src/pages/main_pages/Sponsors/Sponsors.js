75b427f8ca1cfcdf200d8ae66918c9332cb411be
Ashley Tran <73571838+Ashley-Tran@users.noreply.github.com>
2022-10-23 23:45:41 -0700

    Update Sponsors.js

--- src/pages/main_pages/Sponsors/Sponsors.js
+++ src/pages/main_pages/Sponsors/Sponsors.js
@@ -52,19 +52,6 @@ const Sponsors = () => {
 
       {/* </Jumbotron> */}
 
-      <div className="pdf-container">
-        <h1 className = "spons-heading">Fall 2022 Sponsorship Packet</h1>
-        <object
-          class="pdf"
-          width="80%"
-          height="650px"
-          data="../../../../../files/SponsPacketF2022.pdf?#zoom=58&scrollbar=0&toolbar=0&navpanes=0"
-          type="application/pdf"
-        >    
-     
-        </object>
-      </div>
-
 
     </div>
   );
